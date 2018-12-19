c0cd25f11097275202ad86f8b0701a8356e4f2ac
s18010 <s18010@std.it-college.ac.jp>
2018-12-19 16:07:45 +0900

    Monday指定を追記

--- 1219/1219.5.sql
+++ 1219/1219.5.sql
@@ -1,4 +1,5 @@
-select last_name, hire_date, to_char(add_months(hire_date, 6), 'Day", the" Ddspth "of" Month","yyyy') as REVIEW
+select last_name, hire_date, to_char(next_day((add_months(hire_date, 6)), 'Monday'), 'Day", the" Ddspth "of" Month","yyyy')
+as REVIEW
 from employees
 order by employee_id asc
 fetch first 5 rows only;
