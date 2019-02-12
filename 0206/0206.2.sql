5849ed67c59191a6e11227a8a73bcacd6cd93612
s18010 <s18010@std.it-college.ac.jp>
2019-02-12 14:19:33 +0900

    修正

--- 0206/0206.2.sql
+++ 0206/0206.2.sql
@@ -2,15 +2,18 @@
 -- マネージャID、所在地IDを表示
 
 /*
-select department_id, department_name, departments.manager_id, location_id
+select distinct department_id, department_name, departments.manager_id, location_id
 from departments
 join employees using(department_id)
 where job_id != 'SA_REP'
 order by department_id asc;
 */
 
-select department_id, department_name, departments.manager_id, location_id
+select distinct department_id, department_name, departments.manager_id, location_id
 from departments
-where department_id in (select distinct department_id from employees where job_id <> 'SA_REP')
+join employees using(department_id)
+where department_id not in (
+    select department_id from employees
+    where job_id = 'SA_REP'
+    and department_id is not null)
 order by department_id;
-order by department_id asc;
