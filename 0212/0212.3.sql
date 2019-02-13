9d6f59cfe71102e8acf9b29b37edde8f2e3953df
s18010 <s18010@std.it-college.ac.jp>
2019-02-13 15:38:43 +0900

    corrected

--- 0212/0212.3.sql
+++ 0212/0212.3.sql
@@ -1,6 +1,5 @@
 create table title_copy
     (copy_id number(10) constraint title_copy_copy_pk primary key,
-    title_id number(10) constraint title_copy_title_fk preferences title(title_id),
+    title_id number(10) constraint title_copy_title_fk references title(title_id),
     status varchar2(15) not null check(status in ('AVAILABLE', 'DESTROYED', 'RENTED', 'RESERVED'))
     );
-
--- 0212/0212.5.sql
+++ 0212/0212.5.sql
@@ -1,5 +1,5 @@
 create table reservation
     (res_date date constraint resv_res_date_pk primary key,
-    member_id number(10) constraint resv_member_id_fk preferences member(member_id),
-    title_id number(10) constraint resv_title_id_fk preferences title(title_id)
+    member_id number(10) constraint resv_member_id_fk references member(member_id),
+    title_id number(10) constraint resv_title_id_fk references title(title_id)
 );
